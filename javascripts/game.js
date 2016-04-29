e7a50882cc89b2c7c89b0eb94c67b151e3ee8781
Blank0110001 <krishvind23@gmail.com>
2016-04-28 20:58:31 -0400

    Create Ball

--- javascripts/game.js
+++ javascripts/game.js
@@ -8,7 +8,7 @@ ctx.closePath();
 
 function = drawBall() {
   ctx.beginPath();
-  ctx.ellipse(110,110,45,80);
+  ctx.ellipse(0,0,canvas.width,canvas.height);
   ctx.fillStyle = "#FF0000";
   ctx.fill(255,0,0);
   ctx.closePath (); 
