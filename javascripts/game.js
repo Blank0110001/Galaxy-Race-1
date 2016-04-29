bab1397a57dcc261564602ea7699d95bd4174a83
Blank0110001 <krishvind23@gmail.com>
2016-04-28 20:44:52 -0400

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -15,4 +15,82 @@ function drawBall(){
 	ctx.fill();
 	ctx.closePath();
 	}
+function drawPaddle(){
+	ctx.beginPath();
+	ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+	ctx.fillstyle="#000000";
+	ctx.fill();
+	ctx.closePath();
+	}
+function drawBricks() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            if(bricks[c][r].status == 1) {
+                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#FFA500";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function collisionDetection() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
+
+
+function draw(){
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	drawBricks();
+	drawBall();
+	drawPaddle();
+	collisionDetection();
+ if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+			 if(y= y-paddleHeight){
+            dy = -dy  ;
+			 }
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
+    }
+	if(rightPressed && paddleX<canvas.width-paddleWidth){
+		
+		paddleX+=7;
+		}
+	 else if(leftPressed && paddleX>0 ){
+		 paddleX-=7;
+		 
+		 }
+		 
+		 x=x+dx;
+	     y=y+dy;
+	}
+
+setInterval(draw,10);
+Status API Training Shop Blog About
 
