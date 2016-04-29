79cdf216c9a5883dc4238089a5d9530e1eeaf936
Blank0110001 <krishvind23@gmail.com>
2016-04-28 21:11:30 -0400

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -5,10 +5,12 @@ ctx.rect(100,100, 45, 70);
 ctx.fillStyle = "#FF0000";
 ctx.fill(255,0,0);
 ctx.closePath();
-var ballRadius=20
-var ball= draw(){
-  ctx.beginPath();
-  ctx.ellipse(90,110,46,73)
-  ctx.fillStyle = "#FF0000";
-ctx.fill(255,0,0);
-}
+function drawBall(){
+	ctx.beginPath();
+	ctx.arc(10,10,ballRadius,10,10);
+	ctx.fillstyle="#0033FF";
+	ctx.fillStroke="#0033FF";
+	ctx.Stroke="10"
+	ctx.fill();
+	ctx.closePath();
+	}
