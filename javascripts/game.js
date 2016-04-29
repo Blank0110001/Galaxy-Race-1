ec47749b2daf14e45cf8fe1e51b690e4a14459b2
Blank0110001 <krishvind23@gmail.com>
2016-04-28 20:04:02 -0400

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.beginPath();
-ctx.rect(100,100, 70, 70);
+ctx.rect(100,100, 70, 60);
 ctx.fillStyle = "#FF0000";
 ctx.fill(255,0,0);
 ctx.closePath();
