0fa89b7cefb75f3892be97279a4d3fdd59949160
Blank0110001 <krishvind23@gmail.com>
2016-04-30 15:49:02 -0400

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,3 +1,10 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.rect(100,100, 41, 70);
+ctx.fillStyle = "#FF0000";
+ctx.fill(255,0,0);
+ctx.closePath();
 
 var points = [{x:110, y:110}, {x:115, y:98}, {x:120, y:94}];
 var len = points.length;
