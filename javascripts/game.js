80c6ab5aacf9a0536f4fdb9094d88a29a804da37
Blank0110001 <krishvind23@gmail.com>
2016-04-30 15:45:43 -0400

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -5,3 +5,12 @@ ctx.rect(100,100, 45, 70);
 ctx.fillStyle = "#FF0000";
 ctx.fill(255,0,0);
 ctx.closePath();
+
+var points = [{x:45, y:64}, {x:56, y:98}, {x:23, y:44}];
+var len = points.length;
+for(var i = 0; i < len; i++) {
+    alert(points[i].x + ' ' + points[i].y);               
+}
+​
+// to add more points, push an object to the array:
+points.push({x:56, y:87});
